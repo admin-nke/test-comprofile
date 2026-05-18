b31456fa89198ab4002b3e911c06f2f7aeec8f41
iyan-wardiana <iyan-wardiana@gmail.com>
2026-05-18 18:47:37 +0700

    test

--- application/views/index.php
+++ application/views/index.php
@@ -70,6 +70,14 @@
             .img-cover .direktur-k{
                 object-position: right;
             }
+            .fix-header {
+                position: fixed !important;
+                top: 0;
+                left: 0;
+                right: 0;
+                width: 100%;
+                z-index: 999;
+            }
         </style>
 
     </head>
